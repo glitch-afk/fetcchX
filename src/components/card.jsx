f5b2db72b3bcebc67f89481e2553ae6322adf08d
glitch-afk <rahul004@protonmail.ch>
2022-09-19 21:26:48 +0530

    Added animated react islands in feature section

---
+++ src/components/card.jsx
@@ -0,0 +1,46 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+import "swiper/css";
+import "swiper/css/autoplay";
+
+const featureList = [
+  "CHAIN AGNOSTIC",
+  "COMPOSABLE",
+  "Trustless",
+  "Smooth like web2",
+];
+
+const Card = ({ slides, classes, numberOfSlides, delay }) => {
+  return (
+    <Swiper
+      modules={[Autoplay]}
+      slidesPerView={numberOfSlides}
+      direction={"vertical"}
+      centeredSlides={true}
+      loop={true}
+      autoplay={{ delay: delay }}
+      className="h-full select-none shadow-inner"
+    >
+      {slides.map((slide, index) => (
+        <SwiperSlide
+          className={`flex items-center justify-center font-bold select-none ${classes}`}
+          key={index}
+        >
+          {({ isActive }) => (
+            <span
+              className={
+                isActive
+                  ? `bg-gradient-to-b from-[#FF5D46] to-[#FDB658] bg-clip-text text-transparent`
+                  : `text-[#777D8B]/30`
+              }
+            >
+              {slide}
+            </span>
+          )}
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default Card;
